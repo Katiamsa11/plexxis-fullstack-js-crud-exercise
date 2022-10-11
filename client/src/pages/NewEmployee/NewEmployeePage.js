f805b3be102bdbce889d273a11cfa03cab713070
katia santos <katiamsa@gmail.com>
2022-10-11 10:24:53 -0400

    added new employee form validation

--- client/src/pages/NewEmployee/NewEmployeePage.js
+++ client/src/pages/NewEmployee/NewEmployeePage.js
@@ -12,45 +12,88 @@ function NewEmployeePage() {
    TabTitle("Add New");
  //useNavigate to set up backward navigation
   const navigate = useNavigate();
-    // Set State to manipulate the form values
-    const [name, setName] = useState("");
-    const [code, setCode] = useState("");
-    const [city, setCity] = useState("");
-    const [profession, setProfession] = useState("");
-    const [color, setColor] = useState("");
-    const [branch, setBranch] = useState("");
-    const [assigned, setAssigned] = useState("");
+  // Set State to manipulate the form values
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [city, setCity] = useState("");
+  const [profession, setProfession] = useState("");
+  const [color, setColor] = useState("");
+  const [branch, setBranch] = useState("");
+  const [assigned, setAssigned] = useState("");
+
+  //States to generate error messages
+  const [nameError, setNameError] = useState(false);
+  const [codeError, setCodeError] = useState(false);
+  const [cityError, setCityError] = useState(false);
+  const [professionError, setProfessionError] = useState(false);
+  const [colorError, setColorError] = useState(false);
+  const [branchError, setBranchError] = useState(false);
+  const [assignedError, setAssignedError] = useState(false);
       //create state generate success message
   const [success, setSuccess] = useState(false);
   const [isError, setIsError] = useState("");
 
   //handle change to control the form elements
   const handleChangeName = (event) => {
+    if (nameError) {
+      setNameError(false);
+    }
+    setIsError("");
+
     setName(event.target.value);
   };
 
   const handleChangeCode = (event) => {
+    if (codeError) {
+      setCodeError(false);
+    }
+    setIsError("");
 
     setCode(event.target.value);
   };
 
   const handleChangeCity = (event) => {
+    if (cityError) {
+      setCityError(false);
+    }
+    setIsError("");
+
     setCity(event.target.value);
   };
 
   const handleChangeProfession = (event) => {
+    if (professionError) {
+      setProfessionError(false);
+    }
+    setIsError("");
+
     setProfession(event.target.value);
   };
 
   const handleChangeColor = (event) => {
+    if (colorError) {
+      setColorError(false);
+    }
+    setIsError("");
+
     setColor(event.target.value);
   };
 
   const handleChangeBranch = (event) => {
+    if (branchError) {
+      setBranchError(false);
+    }
+    setIsError("");
+
     setBranch(event.target.value);
   };
 
   const handleChangeAssigned = (event) => {
+    if (assignedError) {
+      setAssignedError(false);
+    }
+    setIsError("");
+
     setAssigned(event.target.value);
   };
  
@@ -81,6 +124,28 @@ function NewEmployeePage() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    if (!name) {
+      setNameError(true);
+    }
+    if (!code) {
+      setCodeError(true);
+    }
+    if (!profession) {
+      setProfessionError(true);
+    }
+    if (!color) {
+      setColorError(true);
+    }
+    if (!city) {
+      setCityError(true);
+    }
+    if (!branch) {
+      setBranchError(true);
+    }
+    if (!assigned) {
+      setAssignedError(true);
+    }
+  
     axios
     .post("http://localhost:8080/employees", NewEmployee)
     .then((response) => {
@@ -110,77 +175,139 @@ function NewEmployeePage() {
         <label className="employee-form__label">
           Employee Name
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            nameError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Employee Name"
             value={name}
             onChange={handleChangeName}
           />
+          {nameError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
         <label className="employee-form__label">
         Employee Code
           <input
-            className="employee-form__input"
+          className={`employee-form__input ${
+            codeError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Employee Code"
             value={code}
             onChange={handleChangeCode}
           />
+          {codeError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
         <label className="employee-form__label">
           Employee Profession
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            professionError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="City"
             value={profession}
             onChange={handleChangeProfession}
           />
+          {professionError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
         <label className="employee-form__label">
         Employee Color
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            colorError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Country"
             value={color}
             onChange={handleChangeColor}
           />
+          {colorError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
       </div>
       <div className="employee-form__section">
         <label className="employee-form__label">
           Employee City
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            cityError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Contact Name"
             value={city}
             onChange={handleChangeCity}
           />
+          {cityError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
         <label className="employee-form__label">
         Employee Branch
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            branchError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Employee Branch"
             value={branch}
             onChange={handleChangeBranch}
           />
+          {branchError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
         <label className="employee-form__label">
         Employee Status
           <input
-          className="employee-form__input"
+          className={`employee-form__input ${
+            assignedError ? "employee-form__input--error" : ""
+          }`}
             type="text"
             placeholder="Phone Number"
             value={assigned}
             onChange={handleChangeAssigned}
           />
+          {assignedError && (
+            <p className="error">
+              <img src={errorImg} alt="error icon" className="error__icon" />
+              This field is required
+            </p>
+          )}
         </label>
       </div>
       <footer className="employee-form__footer">
+      <p className="newWarehouse-form__error">{isError}</p>
+          {success && (
+            <p className="newWarehouse-form__error newWarehouse-form__error--success">
+              Employee Successfully Added
+            </p>
+          )}
         <Link
           to="/"
           className="employee-form__button button button--cancel"
